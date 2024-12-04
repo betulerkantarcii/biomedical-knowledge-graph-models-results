870c6b39c0cd19ccf4eb3d5aa50d21f5caa262ef
Betül Erkantarcı <63044081+betulerkantarcii@users.noreply.github.com>
2024-12-04 19:26:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -415,7 +415,8 @@ else:
 #Set the title of your Streamlit app
 st.title("Verifai Project for Validation")
 # Add a description below the title
-st.write(f"This app embeds the Verifai Project application directly into the Streamlit interface. You can search for "Can `{lomefloxacin}` be used in the treatment of `{Acute gonococcal cervicitis}` ?")
+st.write(f"This app embeds the Verifai Project application directly into the Streamlit interface. "
+         f"You can search for 'Can `{lomefloxacin}` be used in the treatment of `{Acute gonococcal cervicitis}`?'")
 
 # Embed the external web app using an iframe
 verifai_url = "https://app.verifai-project.com/main"
