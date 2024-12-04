3cfb092a1ed53903ab66ee36f5e16e7d15c4a6e1
Betül Erkantarcı <63044081+betulerkantarcii@users.noreply.github.com>
2024-12-04 20:09:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -422,7 +422,7 @@ VerifAI BIOMED is a deployed version of VerifAI core, indexing abstracts in the 
 This app embeds the Verifai Project application.
 """, unsafe_allow_html=True) 
 
-st.write(f"`{deneme}`")
+st.write(f"#`{deneme}`")
 
 # Embed the external web app using an iframe
 verifai_url = "https://app.verifai-project.com/main"
